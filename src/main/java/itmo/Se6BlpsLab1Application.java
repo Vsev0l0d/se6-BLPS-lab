ede90ba7b95442dd66afd648121c1b40c32b6c07
Vsev0l0d <vsevolod628@gmail.com>
2022-05-31 13:39:14 +0300

    start lab4

--- src/main/java/itmo/Se6BlpsLab1Application.java
+++ src/main/java/itmo/Se6BlpsLab1Application.java
@@ -1,11 +1,13 @@
 package itmo;
 
+import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
 import org.springframework.amqp.core.Queue;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableProcessApplication
 public class Se6BlpsLab1Application {
 
     public static void main(String[] args) {
